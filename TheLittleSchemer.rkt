48d7a47cbe8241422692c65c6b37e4689763f31a
zzh <zzh@debian.debian.org>
2017-06-20 10:48:55 +0800

    第十章完成

--- TheLittleSchemer.rkt
+++ TheLittleSchemer.rkt
@@ -898,6 +898,294 @@
 
 ;; ;;;09 ...and Again, and Again, and Again,...
 
+(define pick
+  (lambda (n lat)
+    (cond
+      ((null? lat) '())
+      ((zero? (- n 1)) (car lat))
+      (else (pick (- n 1) (cdr lat))))))
+
+(define keep-looking
+  (lambda (a b lat)
+    (cond
+      ((number? b)
+       (cond
+         ((eq? a (pick b lat)) #t)
+         (else (keep-looking a (pick b lat) lat))))
+      (else (eq? a b)))))
+
+(define looking
+  (lambda (a lat)
+    (keep-looking a (pick 1 lat) lat)))
+
+(define build
+  (lambda (a b)
+    (cons a (cons b '()))))
+
+(define shift
+  (lambda (x)
+    (build (first (first x))
+           (build (second (first x))
+                  (second x)))))
+
+(define align
+  (lambda (pora)
+    (cond
+      ((atom? pora) pora)
+      ((a-pair? (first pora)) (align (shift pora)))
+      (else (build (first pora) (align (second pora)))))))
+
+(define length*
+  (lambda (pora)
+    (cond
+      ((atom? pora) 1)
+      (else
+       (+ (length* (first pora))
+          (length* (second pora)))))))
+
+(define weight*
+  (lambda (x)
+    (cond
+      ((atom? x) 1)
+      (else
+       (+ (* (weight* (first x)) 2)
+          (weight* (second x)))))))
+
+(define shuffle
+  (lambda (pora)
+    (cond
+      ((atom? pora) pora)
+      ((a-pair? (first pora)) (shuffle (revpair pora)))
+      (else (build (first pora) (shuffle (second pora)))))))
+
+;;;Y算子部分
+
+(define length
+  (lambda (l)
+    (cond
+      ((null? l) 0)
+      (else (add1 (length (cdr l)))))))
+
+(define y
+  (lambda (le)
+    ((lambda (f) (f f))
+     (lambda (f)
+       (le (lambda (x) ((f f) x)))))))
+
+;;;通过Y算子理解什么是递归 没有看明白
+
+;;;开始第十章
+
+;;;10 What is the Values of All of This?
+
+(define new-entry build)
+
+(define lookup-in-entry-help
+  (lambda (name names values entry-f)
+    (cond
+      ((null? names) (entry-f name))
+      ((eq? name (car names)) (car values))
+      (else
+       (lookup-in-entry name (cdr names) (cdr values) entry-f)))))
+
+(define lookup-in-entry
+  (lambda (name entry entry-f)
+    (lookup-in-entry-help name
+                          (first entry)
+                          (second entry)
+                          entry-f)))
+
+(define extend-table
+  cons)
+
+(define lookup-in-table
+  (lambda (name table table-f)
+    (cond
+      ((null? table) (table-f name))
+      (else
+       (lookup-in-entry name
+                        (car table)
+                        (lambda (name)
+                          (lookup-in-table name
+                                           (cdr table)
+                                           table-f)))))))
+
+(define *const
+  (lambda (e table)
+    (cond
+      ((number? e) e)
+      ((eq? e #t) #t)
+      ((eq? e #f) #f)
+      (else (build 'primitive e)))))
+
+(define text-of
+  second)
+
+(define *quote
+  (lambda (e table)
+    (text-of e)))
+
+(define initial-table
+  (lambda (name)
+    (car (quote ()))))
+
+(define *identifier
+  (lambda (e table)
+    (lookup-in-table e table initial-table)))
+
+(define *lambda
+  (lambda (e table)
+    (build (quote non-primitive)
+           (cons table (cdr e)))))
+
+(define table-of first)
+
+(define formals-of second)
+
+(define body-of third)
+
+(define atom-to-action
+  (lambda (e)
+    (cond
+      ((number? e) *const)
+      ((eq? e #t) *const)
+      ((eq? e #f) *const)
+      ((eq? e (quote cons)) *const)
+      ((eq? e (quote car)) *const)
+      ((eq? e (quote cdr)) *const)
+      ((eq? e (quote null?)) *const)
+      ((eq? e (quote eq?)) *const)
+      ((eq? e (quote atom?)) *const)
+      ((eq? e (quote zero?)) *const)
+      ((eq? e (quote add1)) *const)
+      ((eq? e (quote sub1)) *const)
+      ((eq? e (quote number?)) *const)
+      (else *identifier))))
+
+(define list-to-action
+  (lambda (e)
+    (cond
+      ((atom? (car e))
+       (cond
+         ((eq? (car e) 'quote) *quote)
+         ((eq? (car e) 'lambda) *lambda)
+         ((eq? (car e) 'cond) *cond)
+         (else *application)))
+      (else *application))))
+
+(define expression-to-action
+  (lambda (e)
+    (cond
+      ((atom? e) (atom-to-action e))
+      (else (list-to-action e)))))
+
+(define meaning
+  (lambda (e table)
+    ((expression-to-action e) e table)))
+
+(define else?
+  (lambda (x)
+    (cond
+      ((atom? x) (eq? x (quote else)))
+      (else #f))))
+
+(define question-of first)
+
+(define answer-of second)
+
+(define evcon
+  (lambda (lines table)
+    (cond
+      ((else? (question-of (car lines)))
+       (meaning (answer-of (car lines)) table))
+      ((meaning (question-of (car lines)) table)
+       (meaning (answer-of (car lines)) table))
+      (else (evcon (cdr lines) table)))))
+
+(define cond-line-of cdr)
+
+(define *cond
+  (lambda (e table)
+    (evcon (cond-line-of e) table)))
+
+(define evlis
+  (lambda (args table)
+    (cond
+      ((null? args) (quote ()))
+      (else
+       (cons (meaning (car args) table)
+             (evlis (cdr args) table))))))
+
+(define function-of car)
+
+(define arguments-of cdr)
+
+(define primitive?
+  (lambda (l)
+    (eq? (first l) (quote primitive))))
+
+(define non-primitive?
+  (lambda (l)
+    (eq? (first l) (quote non-primitive))))
+
+(define :atom?
+  (lambda (x)
+    (cond
+      ((atom? x) #t)
+      ((null? x) #t)
+      ((eq? (car x) (quote primitive)) #t)
+      (else #f))))
+
+
+(define apply-primitive
+  (lambda (name vals)
+    (cond
+      ((eq? name (quote cons))
+       (cons (first vals) (second vals)))
+      ((eq? name (quote car))
+       (car (first vals)))
+      ((eq? name (quote cdr))
+       (cdr (first vals)))
+      ((eq? name (quote null?))
+       (null? (first vals)))
+      ((eq? name (quote eq?))
+       (eq? (first vals) (second vals)))
+      ((eq? name (quote atom?))
+       (:atom? (first vals)))
+      ((eq? name (quote zero?))
+       (zero? (first vals)))
+      ((eq? name (quote add1))
+       (add1 (first vals)))
+      ((eq? name (quote sub1))
+       (sub1 (first vals)))
+      ((eq? name (quote number?))
+       (number? (first vals))))))
+
+(define apply-closure
+  (lambda (closure vals)
+    (meaning (body-of closure)
+             (extend-table
+              (new-entry (formals-of closure) vals)
+              (table-of closure)))))
+
+(define apply
+  (lambda (fun vals)
+    (cond
+      ((primitive? fun) (apply-primitive (second fun) vals))
+      ((non-primitive? fun) apply-closure (second fun) vals))))
+
+(define *application
+  (lambda (e table)
+    (apply
+     (meaning (function-of e) table)
+     (evlis (arguments-of e) table))))
+
+
+(define valueA
+  (lambda (e)
+    (meaning e (quote ()))))
+
+
 ;; (define pick
 ;;   (lambda (n lat)
 ;;     (cond
